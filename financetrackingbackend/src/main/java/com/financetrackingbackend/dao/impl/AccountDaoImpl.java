86e5dc70f773256c3aee5fec45b33730ca3df137
Dean Logan <deanlogan42@gmail.com>
2025-02-09 21:41:10 +0000

    Added client for dynamodb in AccountDaoImpl

--- financetrackingbackend/src/main/java/com/financetrackingbackend/dao/impl/AccountDaoImpl.java
+++ financetrackingbackend/src/main/java/com/financetrackingbackend/dao/impl/AccountDaoImpl.java
@@ -2,10 +2,26 @@ package com.financetrackingbackend.dao.impl;
 
 import com.financetrackingbackend.dao.AccountDao;
 import com.financetrackingbackend.schemas.general.Account;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 
 import java.util.Map;
 
+@Component
 public class AccountDaoImpl implements AccountDao {
+    private final DynamoDbTable<Account> accountDynamoDbTable;
+
+    public AccountDaoImpl() {
+        DynamoDbClient dynamoDbClient = DynamoDbClient.create();
+        DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(dynamoDbClient)
+                .build();
+        this.accountDynamoDbTable = enhancedClient.table("Users", TableSchema.fromBean(Account.class));
+    }
+
     @Override
     public Account getAccount(String id) {
         throw new UnsupportedOperationException("Unimplemented method 'getAccount'");
